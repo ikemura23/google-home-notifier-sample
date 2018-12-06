e997342294b8fdc0aca5bc07bcb7371ec39957f5
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2018-12-06 10:59:15 +0900

    update

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var googlehome = require('google-home-notifier');
 var language = 'ja'; // if not set 'us' language will be used
 
-googlehome.device('Google-Home', language); // Change to your Google Home name
+googlehome.device('Google-Home-Hub-2f7308cc5c82a07ace28bb764814eb9d', language); // Change to your Google Home name
 // or if you know your Google Home IP
-googlehome.ip('192.168.3.8', language);
+googlehome.ip('172.16.99.104', language);
 
-googlehome.notify('Hey Foo', function(res) {
+googlehome.notify('ゆめみのイケムラです', function(res) {
   console.log(res);
 });
