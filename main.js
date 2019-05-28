fd5e6ad5e618df12a64f5c04697fe4146d8024aa
ikemura kazutaka <k_ikemura@yumemi.co.jp>
2019-05-28 15:06:18 +0900

    google-home-notifierが動かないので対策版を入れ直す

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 var googlehome = require('google-home-notifier');
 var language = 'ja'; // if not set 'us' language will be used
 
-googlehome.device('Google-Home-Hub-2f7308cc5c82a07ace28bb764814eb9d', language); // Change to your Google Home name
+googlehome.device('Google-Home-Mini-81636ca99c13bbb15b0828a8f55e8e50', language); // Change to your Google Home name
 // or if you know your Google Home IP
-googlehome.ip('172.16.99.104', language);
+// googlehome.ip('172.16.96', language);
 
-googlehome.notify('ゆめみのイケムラです', function(res) {
+googlehome.notify('テストです', function(res) {
   console.log(res);
 });
